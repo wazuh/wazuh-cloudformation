9ba4d7726fdc914a2dc8f8063593b0cd753486d4
Manuel J. Bernal <manu.asi2@gmail.com>
2019-02-20 20:15:49 +0100

    Resources for deploying an agent.

---
+++ wazuh/wazuh_cf_agent.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Install Wazuh master instance using Cloudformation template
+# Support for Amazon Linux
+
+set -exf
+
+ssh_username=$(cat /tmp/wazuh_cf_settings | grep '^SshUsername:' | cut -d' ' -f2)
+master_ip=$(cat /tmp/wazuh_cf_settings | grep '^WazuhMasterIP:' | cut -d' ' -f2)
+elb_wazuh_dns=$(cat /tmp/wazuh_cf_settings | grep '^ElbWazuhDNS:' | cut -d' ' -f2)
+ssh_password=$(cat /tmp/wazuh_cf_settings | grep '^SshPassword:' | cut -d' ' -f2)
+wazuh_server_port=$(cat /tmp/wazuh_cf_settings | grep '^WazuhServerPort:' | cut -d' ' -f2)
+wazuh_registration_password=$(cat /tmp/wazuh_cf_settings | grep '^WazuhRegistrationPassword:' | cut -d' ' -f2)
+wazuh_version=$(cat /tmp/wazuh_cf_settings | grep '^Elastic_Wazuh:' | cut -d' ' -f2 | cut -d'_' -f2)
+
+# Check if running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# Creating SSH user
+adduser ${ssh_username}
+echo "${ssh_username} ALL=(ALL)NOPASSWD:ALL" >> /etc/sudoers
+usermod --password $(openssl passwd -1 ${ssh_password}) ${ssh_username}
+sed -i 's|[#]*PasswordAuthentication no|PasswordAuthentication yes|g' /etc/ssh/sshd_config
+service sshd restart
+
+# Adding Wazuh repository
+wazuh_major_version=$(echo ${wazuh_version} | cut -d'.' -f1)
+cat > /etc/yum.repos.d/wazuh.repo << EOF
+[wazuh_repo]
+gpgcheck=1
+gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
+enabled=1
+name=Wazuh repository
+baseurl=https://packages.wazuh.com/${wazuh_major_version}.x/yum/
+protect=1
+EOF
+
+# Register agent using authd
+/var/ossec/bin/agent-auth -m ${master_ip} -A ubuntu-ag
+sed -i 's:MANAGER_IP:'${elb_wazuh_dns}':g' /var/ossec/etc/ossec.conf
+
+# Installing wazuh-manager
+yum -y install wazuh-agent-${wazuh_version}
+chkconfig --add wazuh-agent
+agent_config="/var/ossec/etc/ossec.conf"
+
+# Change manager protocol to tcp, to be used by Amazon ELB
+sed -i "s/<protocol>udp<\/protocol>/<protocol>tcp<\/protocol>/" ${manager_config}
+
+# Set manager port for agent communications
+sed -i "s/<port>1514<\/port>/<port>${wazuh_server_port}<\/port>/" ${manager_config}
+
+# Setting password for agents registration
+echo "${wazuh_registration_password}" > /var/ossec/etc/authd.pass
+
+# Restart wazuh-manager
+service wazuh-agent restart
