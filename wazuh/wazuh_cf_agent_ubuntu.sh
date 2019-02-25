f1db6a836ce6b064855e01cb13c612d771cdbe11
Manuel J. Bernal <manu.asi2@gmail.com>
2019-02-25 22:14:49 +0100

    Updated AMIs and deploying Ubuntu agent.

--- wazuh/wazuh_cf_agent_ubuntu.sh
+++ wazuh/wazuh_cf_agent_ubuntu.sh
@@ -6,7 +6,6 @@ ssh_username=$(cat /tmp/wazuh_cf_settings | grep '^SshUsername:' | cut -d' ' -f2
 master_ip=$(cat /tmp/wazuh_cf_settings | grep '^WazuhMasterIP:' | cut -d' ' -f2)
 elb_wazuh_dns=$(cat /tmp/wazuh_cf_settings | grep '^ElbWazuhDNS:' | cut -d' ' -f2)
 ssh_password=$(cat /tmp/wazuh_cf_settings | grep '^SshPassword:' | cut -d' ' -f2)
-wazuh_server_port=$(cat /tmp/wazuh_cf_settings | grep '^WazuhServerPort:' | cut -d' ' -f2)
 wazuh_registration_password=$(cat /tmp/wazuh_cf_settings | grep '^WazuhRegistrationPassword:' | cut -d' ' -f2)
 wazuh_version=$(cat /tmp/wazuh_cf_settings | grep '^Elastic_Wazuh:' | cut -d' ' -f2 | cut -d'_' -f2)
 manager_config='/var/ossec/etc/ossec.conf'
@@ -29,12 +28,15 @@ apt-get install curl apt-transport-https lsb-release -y
 
 # Install Wazuh agent
 apt-get update
-apt-get install wazuh-agent-${wazuh_version} -y
+apt-get install wazuh-agent -y
+
+# Add registration password
+echo "${wazuh_registration_password}" > /var/ossec/etc/authd.pass
 
 # Register agent using authd
 /var/ossec/bin/agent-auth -m ${master_ip} -A ubuntu-ag
 sed -i 's:MANAGER_IP:'${elb_wazuh_dns}
 # Enable and restart the Wazuh agent
-systemctl daemon-reload
+
 systemctl enable wazuh-agent
 systemctl restart wazuh-agent
