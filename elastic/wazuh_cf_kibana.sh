228f91d0a96c2c955b8ccf35d1682782bc7248e7
manuasir <manuel.jimenez@wazuh.com>
2019-09-10 19:09:13 +0200

    Testing RC1 package

--- elastic/wazuh_cf_kibana.sh
+++ elastic/wazuh_cf_kibana.sh
@@ -255,7 +255,7 @@ get_plugin_url(){
   if [[ ${EnvironmentType} == 'staging' ]]
   then
     # Adding Wazuh pre_release repository
-  plugin_url="https://packages-dev.wazuh.com/staging/app/kibana/wazuhapp-${wazuh_major}.${wazuh_minor}.${wazuh_patch}_${elastic_major_version}.${elastic_minor_version}.${elastic_patch_version}-rc2.zip"
+  plugin_url="https://packages-dev.wazuh.com/staging/app/kibana/wazuhapp-3.10.0_7.3.1-rc1.zip"
   elif [[ ${EnvironmentType} == 'production' ]]
   then
   plugin_url="https://packages.wazuh.com/wazuhapp/wazuhapp-${wazuh_major}.${wazuh_minor}.${wazuh_patch}_${elastic_major_version}.${elastic_minor_version}.${elastic_patch_version}.zip"
