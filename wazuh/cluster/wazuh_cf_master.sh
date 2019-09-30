d6b765913e89f4d64abbec1e93dfa0fb357c49cc
manuasir <manuel.jimenez@wazuh.com>
2019-09-30 17:03:50 +0200

    Updated OSS resource

--- wazuh/cluster/wazuh_cf_master.sh
+++ wazuh/cluster/wazuh_cf_master.sh
@@ -69,8 +69,8 @@ else
 fi
 
 # Configuring Filebeat-oss repository
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-${elastic_version}-linux-x86_64.tar.gz
-yum install -y filebeat-oss-${elastic_version}-x86_64.rpm
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${elastic_version}-x86_64.rpm
+sudo rpm -vi filebeat-${elastic_version}-x86_64.rpm
 
 # Installing wazuh-manager
 yum -y install wazuh-manager
