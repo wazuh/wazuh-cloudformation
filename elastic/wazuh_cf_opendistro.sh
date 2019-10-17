928eb90b640e423ae33bc346e95b279607581a33
Manuel J. Bernal <manuel.jimenez@wazuh.com>
2019-10-17 11:48:29 +0200

    Opendistro CCS environment (#25)
    Added support for a testing CCS Opendistro-based architecture. 
    - One CCS coordinating node with Kibana + Elasticsearch
    - Two separated Opendistro elasticsearch clusters + Wazuh manager

--- elastic/wazuh_cf_opendistro.sh
+++ elastic/wazuh_cf_opendistro.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Install Kibana instance using Cloudformation template
-# Support for Amazon Linux
+# Install Elastic data node using Cloudformation template
 
-echo "Starting process." >> /tmp/deploy.log
+touch /tmp/deploy.log
 
+echo "OpenDistro: Starting process." > /tmp/deploy.log
 ssh_username=$(cat /tmp/wazuh_cf_settings | grep '^SshUsername:' | cut -d' ' -f2)
 ssh_password=$(cat /tmp/wazuh_cf_settings | grep '^SshPassword:' | cut -d' ' -f2)
+echo "Added env vars." >> /tmp/deploy.log
 
 check_root(){
-    echo "Checking root." >> /tmp/deploy.log
     # Check if running as root
     if [[ $EUID -ne 0 ]]; then
         echo "NOT running as root. Exiting" >> /tmp/deploy.log
@@ -24,31 +24,19 @@ create_ssh_user(){
     echo "${ssh_username} ALL=(ALL)NOPASSWD:ALL" >> /etc/sudoers
     usermod --password $(openssl passwd -1 ${ssh_password}) ${ssh_username}
     echo "Created SSH user." >> /tmp/deploy.log
+
     sed -i 's|[#]*PasswordAuthentication no|PasswordAuthentication yes|g' /etc/ssh/sshd_config
     systemctl restart sshd
     echo "Started SSH service." >> /tmp/deploy.log
 }
 
-await_kibana(){
-  echo "Waiting for Kibana service..." >> /tmp/deploy.log
-  until curl -XGET "http://$eth0_ip:5601" -k -u elastic:${ssh_password}; do
-      sleep 5
-      echo "Kibana not ready yet..." >> /tmp/deploy.log
-  done
-  echo "Kibana is up" >> /tmp/deploy.log
-}
-
 import_opendistro_repo(){
 # Configuring Elastic repository
 curl https://d3g5vo6xdbdb9a.cloudfront.net/yum/opendistroforelasticsearch-artifacts.repo -o /etc/yum.repos.d/opendistroforelasticsearch-artifacts.repo
+
 echo "Added OpenDistro repo." >> /tmp/deploy.log
 }
 
-install_java(){
-    amazon-linux-extras install java-openjdk11 
-}
-
-# Installing ELK coordinating only mode
 install_opendistro(){
     echo "Installing OpenDistro." >> /tmp/deploy.log
     # Installing OpenDistro
@@ -56,34 +44,21 @@ install_opendistro(){
     echo "Installed OpenDistro." >> /tmp/deploy.log
 }
 
+install_java(){
+    amazon-linux-extras install java-openjdk11 
+}
+
 start_opendistro(){
     systemctl start elasticsearch.service
 }
 
-
-install_kibana(){
-  # Installing Kibana
-  yum install opendistroforelasticsearch-kibana -y
-  echo "Kibana installed." >> /tmp/deploy.log
-}
-
-start_kibana(){
-  # Starting Kibana
-  systemctl restart kibana
-  await_kibana
-}
-
-
 main(){
-  check_root
-  create_ssh_user
-  import_opendistro_repo
-  install_java
-  install_opendistro
-  configuring_elasticsearch
-  start_opendistro
-  install_kibana
-  start_kibana
+    check_root
+    create_ssh_user
+    install_java
+    import_opendistro_repo
+    install_opendistro
+    start_opendistro
 }
 
 main
