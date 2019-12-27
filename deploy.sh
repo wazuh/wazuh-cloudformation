c5d69227704baf185b8008651b6d0c50dc039185
Jose M <jm.garcia@wazuh.com>
2019-12-27 12:36:34 +0100

    Change default stack name

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ PARAMS_FILE='./parameters.json'
 TEMPLATE_FILE='./wazuh_template.yml'
 
 # Stack name
-STACK_NAME='demo-info'
+STACK_NAME='wazuh-environment'
 
 # Bucket name
 BUCKET_NAME='demo-cloudformation-templates'
