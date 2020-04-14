952c477aa910ec9ea8333d71025360a0f8f80d7e
Zenidd <jpsaezgutierrez@gmail.com>
2020-04-14 11:57:10 +0200

    minor optimize fix

--- elastic/wazuh_cf_kibana.sh
+++ elastic/wazuh_cf_kibana.sh
@@ -279,7 +279,7 @@ install_plugin(){
 optimize_kibana(){
   echo "Optimizing app" >> /tmp/deploy.log
   cd /usr/share/kibana
-  NODE_OPTIONS="--max-old-space-size=3072" /usr/share/kibana/bin/kibana --allow-root > /var/log/kibana_error.log
+  NODE_OPTIONS="--max-old-space-size=3072" /usr/share/kibana/bin/kibana --optimize --allow-root > /var/log/kibana_error.log
   echo "App optimized!" >> /tmp/deploy.log
 }
 
