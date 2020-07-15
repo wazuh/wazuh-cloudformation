f1418b31f4843fe64953a535b9c69115324cfd73
Zenidd <jpsaezgutierrez@gmail.com>
2020-07-15 11:46:27 +0200

    fixes

--- demo/elastic/wazuh_cf_kibana.sh
+++ demo/elastic/wazuh_cf_kibana.sh
@@ -311,8 +311,8 @@ optimize_kibana(){
 
 add_api(){
 echo "Adding Wazuh API" >> /tmp/deploy.log
-sed -ie '/- default:/,+4d' /usr/share/kibana/optimize/wazuh/config/wazuh.yml
 cat > /usr/share/kibana/optimize/wazuh/config/wazuh.yml << EOF
+#${wazuh_master_ip}
 hosts:
   - default:
       url: https://${wazuh_master_ip}
