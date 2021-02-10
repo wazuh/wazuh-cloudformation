ce65f990149c511e9ae7ade7e38a650340079cd1
Sergio García <garcapradossergi76@gmail.com>
2021-02-10 18:23:55 +0100

    Fix dst log

--- distributed/unattended/templates/test_files.sh
+++ distributed/unattended/templates/test_files.sh
@@ -8,7 +8,7 @@ do
     if [[ -f "$CONFIG_FILE" ]] && [[ -f "$ELASTIC_INSTALL_SCRIPT" ]]
     then
         find=1
-        logger -s "Files found. Moving them to /root..." 2>> {{log_file}}
+        logger -s "Files found. Moving them to {{dst}}..." 2>> {{log_file}}
         mv $CONFIG_FILE {{dst}}/config.yml
         mv $ELASTIC_INSTALL_SCRIPT {{dst}}/elastic-stack-installation.sh
     else
