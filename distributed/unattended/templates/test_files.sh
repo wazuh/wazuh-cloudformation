babec26c599e0889da2e491b31bb24c95ab747b1
Sergio García <garcapradossergi76@gmail.com>
2021-02-10 18:36:36 +0100

    Fix file type

--- distributed/unattended/templates/test_files.sh
+++ distributed/unattended/templates/test_files.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-CONFIG_FILE={{src}}/config.yml
+CERTS_FILE={{src}}/certs.tar
 ELASTIC_INSTALL_SCRIPT={{src}}/elastic-stack-installation.sh
 
 find=0
 while [ $find -eq 0 ]
 do
-    if [[ -f "$CONFIG_FILE" ]] && [[ -f "$ELASTIC_INSTALL_SCRIPT" ]]
+    if [[ -f "$CERTS_FILE" ]] && [[ -f "$ELASTIC_INSTALL_SCRIPT" ]]
     then
         find=1
         logger -s "Files found. Moving them to {{dst}}..." 2>> {{log_file}}
-        mv $CONFIG_FILE {{dst}}/config.yml
+        mv $CERTS_FILE {{dst}}/certs.tar
         mv $ELASTIC_INSTALL_SCRIPT {{dst}}/elastic-stack-installation.sh
     else
         logger -s "Files not found. Sleeping 10 seconds..." 2>> {{log_file}}
