a677aa4ac058ecb58641072180ddaa2147a1ee9f
Sergio García <garcapradossergi76@gmail.com>
2021-02-11 14:15:11 +0100

    Check whether file already in /root

--- distributed/unattended/templates/check_certs.sh
+++ distributed/unattended/templates/check_certs.sh
@@ -4,11 +4,15 @@ CERTS_FILE={{src}}/certs.tar
 find=0
 while [ $find -eq 0 ]
 do
-    if [[ -f "$CERTS_FILE" ]]
+    if [[ -f "{{dst}}/certs.tar" ]]
+    then
+        find=1
+        echo "Cert files already in /root"
+    elif [[ -f "$CERTS_FILE" ]]
     then
         find=1
         echo "Cert files found. Moving them to {{dst}}..." 
-        mv $CERTS_FILE {{dst}}/certs.tar
+        mv $CERTS_FILE {{dst}}
     else
         echo "Cert files not found. Sleeping 10 seconds..."
         sleep 10
