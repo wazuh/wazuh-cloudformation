7afab6f62ef9d338b2613ac145b18925f3978668
Sergio García <garcapradossergi76@gmail.com>
2021-02-12 12:39:51 +0100

    Add check_ports.sh

---
+++ distributed/unattended/templates/check_ports.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER=$1
+IP=$2
+PORT=$3
+
+open=0
+while [ $open -eq 0 ]
+do
+    ssh -i ~/.ssh/ssh.key $USER@$IP "netstat -tulnp" | grep $PORT
+    if [[ $? -eq 0 ]] # Open
+    then
+        open=1
+        echo "Port $PORT open in $IP"
+    else # Close
+        echo "Port $PORT close in $IP. Sleeping 10 seconds..."
+        sleep 10
+    fi
+done
+
