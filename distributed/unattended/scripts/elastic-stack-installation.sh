e4ec4f3f80077445d0c3a874e1fb27afe530aa28
Sergio García <garcapradossergi76@gmail.com>
2021-02-25 11:51:59 +0100

    Force setcap root

--- distributed/unattended/scripts/elastic-stack-installation.sh
+++ distributed/unattended/scripts/elastic-stack-installation.sh
@@ -453,7 +453,7 @@ installKibana() {
             echo "Error: Wazuh Kibana plugin could not be installed."
             exit 1;
         fi
-        eval "setcap 'cap_net_bind_service=+ep' /usr/share/kibana/node/bin/node | logger -s 2>> /var/log/wazuh-cloudformation.log"
+        eval "sudo -u root setcap 'cap_net_bind_service=+ep' /usr/share/kibana/node/bin/node"
         if [ "$?" != 0 ]; then
 		    echo "Error: setcap 443 port"
 	    else
