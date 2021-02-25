03a4960c842389e4b88c176cb7c42df277b9c86e
Sergio García <garcapradossergi76@gmail.com>
2021-02-25 11:37:17 +0100

    log setcap command

--- distributed/unattended/scripts/elastic-stack-installation.sh
+++ distributed/unattended/scripts/elastic-stack-installation.sh
@@ -431,7 +431,7 @@ installKibana() {
 
     if [[ -f /etc/kibana/kibana.yml ]]; then
         echo "Kibana is already installed in this node."
-        #exit 1;
+        exit 1;
     fi
 
     logger "Installing Kibana..."
@@ -442,7 +442,7 @@ installKibana() {
     fi
     if [  "$?" != 0  ]; then
         echo "Error: Kibana installation failed"
-        #exit 1;
+        exit 1;
     else
         eval "curl -so /etc/kibana/kibana.yml https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/open-distro/unattended-installation/distributed/templates/kibana_unattended.yml --max-time 300 ${debug}"
         eval "mkdir /usr/share/kibana/data ${debug}"
