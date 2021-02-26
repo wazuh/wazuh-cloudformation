8e14d9f680007ddbfd475bd08efb5b114df6e7c3
Sergio García <garcapradossergi76@gmail.com>
2021-02-26 11:56:51 +0100

    Add log_file var

--- distributed/unattended/scripts/check_kibana.sh
+++ distributed/unattended/scripts/check_kibana.sh
@@ -5,12 +5,12 @@ active=0
 while [[ $active -eq 0 ]]; do
     systemctl is-active --quiet kibana.service
     if [ "$?" != 0 ]; then
-        echo "Kibana service disable. Sleeping 60 seconds..."
+        echo "Kibana service disable. Sleeping 60 seconds..." | logger -s 2>> {{log_file}}
         sleep 60
     else
-        echo "Kibana service enable. Linking 443 port to kibana socket..."
+        echo "Kibana service enable. Linking 443 port to kibana socket..." | logger -s 2>> {{log_file}}
         setcap 'cap_net_bind_service=+ep' /usr/share/kibana/node/bin/node
-        echo "Restarting kibana service..."
+        echo "Restarting kibana service..." | logger -s 2>> {{log_file}}
         systemctl restart kibana.service
         active=1
     fi
