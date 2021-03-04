bde813380a06c643d7ce74bb6c718b6cad09f958
Sergio García <garcapradossergi76@gmail.com>
2021-03-04 15:58:29 +0100

    Update kibana script

--- distributed/unattended/scripts/initialize_kibana.sh
+++ distributed/unattended/scripts/initialize_kibana.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo "Linking 443 port to Kibana socket..."
+setcap 'cap_net_bind_service=+ep' /usr/share/kibana/node/bin/node
+echo "Starting kibana service..."
+systemctl daemon-reload
+systemctl enable kibana.service
+systemctl restart kibana.service
 echo "Initializing Kibana (this may take a while)"
 until [[ "$(curl -XGET https://{{kibana_ip}}/status -I -uadmin:admin -k -s --max-time 300 | grep "200 OK")" ]]; do
-    echo -ne "."
     sleep 10
 done
 conf="$(awk '{sub("url: https://localhost", "url: https://{{wazuh_master_ip}}")}1' /usr/share/kibana/data/wazuh/config/wazuh.yml)"
