0066f0a6c7494d7fda005f9c97dfb600da24f888
Sergio García <garcapradossergi76@gmail.com>
2021-03-04 16:29:40 +0100

    Fix quote

--- distributed/unattended/scripts/initialize_kibana.sh
+++ distributed/unattended/scripts/initialize_kibana.sh
@@ -12,4 +12,4 @@ until [[ "$(curl -XGET https://{{kibana_ip}}/status -I -uadmin:admin -k -s --max
 done
 conf="$(awk '{sub("url: https://localhost", "url: https://{{wazuh_master_ip}}")}1' /usr/share/kibana/data/wazuh/config/wazuh.yml)"
 echo "${conf}" > /usr/share/kibana/data/wazuh/config/wazuh.yml  
-echo "You can access the web interface https://{{kibana_ip}}. The credentials are admin:admin'    
+echo "You can access the web interface https://{{kibana_ip}}. The credentials are admin:admin"  
